59a2974ba730f2c0b8c1583d16b9bf46934d235f
AdrianGerman <adrian.german1019@gmail.com>
2025-03-18 08:06:56 -0600

    main component

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,11 @@
+export const MainComponents = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="text-4xl font-bold">Biblioteca de componentes</h1>
+      <p className="text-md w-96">
+        Un conjunto de componentes accesibles y de diseño atractivo. Compatible
+        con tus frameworks favoritos y de código abierto.
+      </p>
+    </div>
+  )
+}
